8c78e42a8027b21ef86581dbff038fd949076bee
Krishna Prakash <krishnaprakash.tg@litmus7.com>
2024-10-19 15:35:42 +0530

    API integration

---
+++ packages/app/features/home/product-home-screen.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+import { getProducts } from "app/api/products";
+import { Product } from "app/types/product";
+
+import { Text } from "app/design/typography"; 
+import { View } from "app/design/view"; 
+
+import { FlatList, ListRenderItem } from "react-native";
+
+const ProductHomeScreen = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    getProducts().then((data) => {
+      setProducts(data);
+    });
+  }, []);
+
+  const renderItem: ListRenderItem<Product> = ({ item }) => (
+    <View> 
+      <Text>{item.title}</Text>
+      <Text>{item.price}</Text>
+    </View>
+  );
+  return (
+    <View>
+        <Text>All products</Text>
+        {products.length > 0 &&
+            products.map(p => {
+                return <Text>{p.title}</Text>
+            })
+        }
+      {/* <FlatList
+        data={products}
+        renderItem={renderItem}
+        keyExtractor={(item: Product) => item.id.toString()}
+      /> */}
+    </View>
+  );
+};
+
+export default ProductHomeScreen;
