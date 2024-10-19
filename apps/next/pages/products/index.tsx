11ec34139bd830e01a6e4e1639b8adb57ff76d8b
Krishna Prakash <krishnaprakash.tg@litmus7.com>
2024-10-19 15:57:58 +0530

    minor change

--- apps/next/pages/products/index.tsx
+++ apps/next/pages/products/index.tsx
@@ -1,35 +1,35 @@
-import { getProducts } from "app/api/products"
+import { getProducts } from "app/api/products";
 import { Product } from "app/types/product";
 import Image from "next/image";
 
-const Products = ({ products }: {products: [Product]}) => { 
-    
+const Products = ({ products }: { products: [Product] }) => {
   return (
     <div>
-        <h1 className="px-5">All products</h1>
-        <div className="grid grid-cols-4 gap-6 p-5">
-        {
-           products.map( p => 
-            <div key={p.id}>
-                <Image 
-                    src={p.thumbnail}
-                    width={100}
-                    height={100}
-                    alt="Picture of the author"
-                />
-                <p>{p.title}</p>
-                <p><strong>$</strong>{p.price}</p>
-            </div>
-           ) 
-        }
-        </div>
+      <h1 className="px-5">All products</h1>
+      <div className="grid grid-cols-4 gap-6 p-5">
+        {products.map((p) => (
+          <div key={p.id}>
+            <Image
+              src={p.thumbnail}
+              width={100}
+              height={100}
+              alt={p.title}
+            />
+            <p>{p.title}</p>
+            <p>
+              <strong>$</strong>
+              {p.price}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
+};
+
+export async function getStaticProps() {
+  const products = await getProducts();
+  return { props: { products } };
 }
 
-export async function getStaticProps() { 
-    const products = await getProducts()
-    return { props: { products } }
-  }
-
-export default Products
+export default Products;
